7442cce36694835f152d6ae49b84f6085b38fa4d
Mario Salvatore <xauron567x@gmail.com>
2022-06-23 12:26:19 +0200

    Add more test to beer detail view model

--- app/src/test/java/com/example/beerstest/presentation/beerdetail/BeerDetailViewModelTest.kt
+++ app/src/test/java/com/example/beerstest/presentation/beerdetail/BeerDetailViewModelTest.kt
@@ -11,7 +11,11 @@ import com.example.beerstest.utils.createBeerEntity
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
 import io.mockk.MockKAnnotations
+import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -20,9 +24,9 @@ class BeerDetailViewModelTest : ViewModelTest() {
 
     @MockK(relaxed = true) lateinit var isFavoriteUseCase: IsFavoriteUseCase
 
-    @MockK lateinit var saveFavoriteUseCase: SaveFavoriteUseCase
+    @MockK(relaxed = true) lateinit var saveFavoriteUseCase: SaveFavoriteUseCase
 
-    @MockK lateinit var removeFavoriteUseCase: RemoveFavoriteUseCase
+    @MockK(relaxed = true) lateinit var removeFavoriteUseCase: RemoveFavoriteUseCase
 
     private lateinit var viewModel: BeerDetailViewModel
 
@@ -32,13 +36,113 @@ class BeerDetailViewModelTest : ViewModelTest() {
     }
 
     @Test
-    fun `view model set beer on init`() = runTest {
+    fun `view model set proper beer on init`() = runTest {
         // Init view model
         val beer = createBeerEntity()
         setupViewModel(beer)
 
         // Assertion
         viewModel.currentState.currentBeer shouldBe beer
+    }
+
+    @Test
+    fun `view model set proper favorite on init`() = runTest {
+        // Stub use case
+        val isFavorite = true
+        val flow = flow { emit(isFavorite) }
+        every { isFavoriteUseCase.invoke(any()) } returns flow
+
+        // Init view model
+        setupViewModel(createBeerEntity())
+
+        // Assertion
+        viewModel.currentState.isFavorite shouldBe isFavorite
+    }
+
+    @Test
+    fun `view model call use case with proper id when check favorite`() = runTest {
+        // Init view model
+        val beer = createBeerEntity(id = 8)
+        setupViewModel(beer)
+
+        // Verify
+        verify {
+            isFavoriteUseCase.invoke(beer.id)
+        }
+    }
+
+    @Test
+    fun `view model call use case with proper id when remove favorite`() = runTest {
+        // Stub use case
+        val flow = flow { emit(true) }
+        every { isFavoriteUseCase.invoke(any()) } returns flow
+
+        // Init view model
+        val beer = createBeerEntity(id = 10)
+        setupViewModel(beer)
+
+        // Call view model
+        viewModel.launchEvent(BeerDetailContract.Event.OnFavoriteClicked)
+
+        // Verify
+        coVerify {
+            removeFavoriteUseCase.invoke(beer.id)
+        }
+    }
+
+    @Test
+    fun `view model call use case with proper beer when save favorite`() = runTest {
+        // Stub use case
+        val flow = flow { emit(false) }
+        every { isFavoriteUseCase.invoke(any()) } returns flow
+
+        // Init view model
+        val beer = createBeerEntity()
+        setupViewModel(beer)
+
+        // Call view model
+        viewModel.launchEvent(BeerDetailContract.Event.OnFavoriteClicked)
+
+        // Verify
+        coVerify {
+            saveFavoriteUseCase.invoke(beer)
+        }
+    }
+
+    @Test
+    fun `view model launch proper effect when favorite is removed`() = runTest {
+        // Stub use case
+        val flow = flow { emit(true) }
+        every { isFavoriteUseCase.invoke(any()) } returns flow
+
+        // Init view model
+        setupViewModel(createBeerEntity())
+
+        // Call view model
+        viewModel.launchEvent(BeerDetailContract.Event.OnFavoriteClicked)
+
+        // Assertion
+        viewModel.effect.test {
+            awaitItem().shouldBeTypeOf<BeerDetailContract.Effect.FavoriteRemoved>()
+        }
+    }
+
+    @Test
+    fun `view model launch proper effect when favorite is saved`() = runTest {
+        // Stub use case
+        val flow = flow { emit(false) }
+        every { isFavoriteUseCase.invoke(any()) } returns flow
+
+        // Init view model
+        setupViewModel(createBeerEntity())
+
+        // Call view model
+        viewModel.launchEvent(BeerDetailContract.Event.OnFavoriteClicked)
+
+        // Assertion
+        viewModel.effect.test {
+            awaitItem().shouldBeTypeOf<BeerDetailContract.Effect.FavoriteSaved>()
+        }
     }
 
     @Test
